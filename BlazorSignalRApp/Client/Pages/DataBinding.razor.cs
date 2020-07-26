a5a03788363ade2f49b5563308ddd9463f7851fc
trickyrat <wangjian0513@outlook.com>
2020-07-26 17:06:25 +0800

    add data binding sample

---
+++ BlazorSignalRApp/Client/Pages/DataBinding.razor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSignalRApp.Client.Pages
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        [Display(Name = "男")]
+        Male = 1,
+        [Display(Name = "女")]
+        Female = 2
+    }
+    public partial class DataBinding
+    {
+        private Person personModel = new Person();
+
+        private void HandleValidSubmit()
+        {
+            Console.WriteLine("OnValidSubmit");
+        }
+
+
+        public int CurrentValue { get; set; }
+
+        public void Count()
+        {
+            CurrentValue++;
+        }
+    }
+}
